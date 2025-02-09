e90870c6a68c45466754c872cb29bfb4f4e37081
joaquin <jpinilloslr@gmail.com>
2025-02-09 22:18:50 +0100

    update background and scene

--- background.asm
+++ background.asm
@@ -1,51 +1,70 @@
 .include "nes.inc"
+.include "scene.inc"
 
 .export load_background
+
+.segment "ZEROPAGE"
+  background_ptr: .res 2
 
 .segment "CODE"
 
 .proc load_background
-  lda PPU_STATUS         ; Reset PPU toggle (read PPU_STATUS)
-  lda #$20               ; Set PPU address high byte ($2000 - Start of Name Table 0)
+  lda PPU_STATUS         ; Reset PPU toggle
+  lda #$20
   sta PPU_ADDR
-  lda #$00               ; Low byte (start at the beginning of the name table)
-  sta PPU_ADDR
+  lda #$00
+  sta PPU_ADDR           ; Set PPU address to Name Table 0
 
-  ldx #$04               ; Outer loop counter (4 pages of 256 bytes)
-  ldy #$00               ; Inner loop counter (256 bytes per page)
-  lda #04                ; Tile index to fill the background with
+  
+  lda #<scene_tilemap
+  sta background_ptr
+  lda #>scene_tilemap
+  sta background_ptr + 1 ; Set up a pointer to the tilemap in RODATA
+
+  ldx #04                ; Outer loop counter (4 pages of 256 bytes)
+  ldy #00                ; Inner loop counter (256 bytes per page)
 
 @loop:
-  sta PPU_DATA           ; Write tile to PPU
-  dey                    ; Decrement inner loop counter
-  bne @loop              ; Continue inner loop until Y wraps around to 0
+  lda (background_ptr), y
+  sta PPU_DATA
+  iny
+  bne @loop
+
+  inc background_ptr + 1 ; Increment high byte of pointer to move to next page
   dex                    ; Decrement outer loop counter
   bne @loop              ; Continue outer loop until X wraps around to 0
 
   lda #0
-  sta PPU_SCROLL         ; Reset horizontal scroll
-  sta PPU_SCROLL         ; Reset vertical scroll
-  jmp LoadAttributeTable ; Load the attribute table
+  sta PPU_SCROLL
+  sta PPU_SCROLL
+  jmp load_attribute_table
 .endproc
 
-.proc LoadAttributeTable
-  lda PPU_STATUS         ; Reset PPU latch
+.proc load_attribute_table
+  lda PPU_STATUS               ; Reset PPU latch
 
-  lda #$23               ; Set PPU address to $23C0 (Start of Attribute Table)
+  lda #$23
   sta PPU_ADDR
   lda #$C0
-  sta PPU_ADDR
+  sta PPU_ADDR                 ; Set PPU address to Name Table 0
+  
+  lda #<scene_attribute_table
+  sta background_ptr
+  lda #>scene_attribute_table  ; Set up a pointer to the attribute_table in RODATA
+  sta background_ptr+1
 
-  ldx #$00
+  ldy #$00
 @loop:
-  lda #$00               ; Use Background Palette 0 for all 2x2 tile blocks
+  lda (background_ptr), y
   sta PPU_DATA
-  inx
-  cpx #$40               ; 64 bytes for the full Attribute Table
+  iny
+  cpy #54                      ; Check if we've written 64 bytes (size of the attribute table)
   bne @loop
 
+  ; Reset scroll registers
   lda #0
-  sta PPU_SCROLL         ; Reset horizontal scroll
-  sta PPU_SCROLL         ; Reset vertical scroll
+  sta PPU_SCROLL
+  sta PPU_SCROLL
+
   rts
 .endproc
